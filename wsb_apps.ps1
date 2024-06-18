a5cfdbf3443fa742bafb03784caa535a86ce1cd4
junghunjoe <93698182+junghunjoe@users.noreply.github.com>
2024-06-18 22:22:36 +0900

    Update wsb_apps.ps1

--- wsb_apps.ps1
+++ wsb_apps.ps1
@@ -5,4 +5,4 @@ irm https://community.chocolatey.org/install.ps1 | iex
 RefreshEnv
 Set-Location 'C:\ProgramData\chocolatey\bin'
 .\Choco.exe feature enable -n allowGlobalConfirmation
-.\Choco.exe install C:\Users\WDAGUtilityAccount\Downloads\packages.config -y
+.\Choco.exe install C:\Users\WDAGUtilityAccount\Downloads\wsb_packages.config -y
