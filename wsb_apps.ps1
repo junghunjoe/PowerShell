d3a18815264c8af707488bf2bb31fa3a5d17604f
junghunjoe <93698182+junghunjoe@users.noreply.github.com>
2024-06-18 22:17:00 +0900

    Update wsb_apps.ps1

--- wsb_apps.ps1
+++ wsb_apps.ps1
@@ -5,7 +5,4 @@ irm https://community.chocolatey.org/install.ps1 | iex
 RefreshEnv
 Set-Location 'C:\ProgramData\chocolatey\bin'
 .\Choco.exe feature enable -n allowGlobalConfirmation
-.\Choco.exe install GoogleChrome -y --ignore-check
-.\Choco.exe install firefox -y
-.\Choco.exe install vlc -y
-.\Choco.exe install winscp -y
+.\Choco.exe install C:\Users\WDAGUtilityAccount\Downloads\packages.config -y
