45da5f24c5f9aa98e71284bbbee8d235fc2b17da
junghunjoe <93698182+junghunjoe@users.noreply.github.com>
2024-06-18 22:59:35 +0900

    Update wsb_apps.ps1

--- wsb_apps.ps1
+++ wsb_apps.ps1
@@ -6,3 +6,5 @@ RefreshEnv
 Set-Location 'C:\ProgramData\chocolatey\bin'
 .\Choco.exe feature enable -n allowGlobalConfirmation
 .\Choco.exe install C:\Users\WDAGUtilityAccount\Downloads\wsb_packages.config -y
+
+Start-Process -FilePath "C:\Users\WDAGUtilityAccount\Videos\Captures\DLA\Channel_Updates_Download.bat" -Wait
